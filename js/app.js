865acb919727a121f3ccc4581d4fcef34ed0460c
chloe alcorn <ca.alcorn@outlook.com>
2018-09-07 15:23:03 +0100

    breed , age, name , sex added to input form

--- js/app.js
+++ js/app.js
@@ -7,5 +7,36 @@ document.addEventListener('DOMContentLoaded',() => {
 const handleNewItemFormSubmit = function(event){
   event.preventDefault();
 
+const animalListItem = createAnimalListItem(event.target)
+const animalList = document.querySelector('#animal-list');
+animalList.appendChild(animalListItem);
+
+
+event.target.reset();
+
+}
+
+const createAnimalListItem = function(form){
+  const animalListItem = document.createElement('li');
+  animalListItem.classList.add('animal-list-item');
+
+  const name = document.createElement('h3');
+  name.textContent = `Name: ${form.name.value}` ;
+  animalListItem.appendChild(name)
+
+  const age = document.createElement('h3');
+  age.textContent = `Age: ${form.age.value}`;
+  animalListItem.appendChild(age)
+
+  const breed = document.createElement('h3');
+  breed.textContent = `Breed: ${form.breed.value}`;
+  animalListItem.appendChild(breed);
+
+  const sex = document.createElement('h3');
+  sex.textContent = form.sex.value;
+  animalListItem.appendChild(sex);
+
+  return animalListItem;
+
 
 }
